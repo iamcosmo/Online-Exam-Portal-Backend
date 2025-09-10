2b64b4264a1981fa2ff8ab1169dafb1e18390c0d
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-09-10 18:14:58 +0530

    update : repository functions for getexams,addexams added

--- Infrastructure/Repositories/Implementations/ExamRepository.cs
+++ Infrastructure/Repositories/Implementations/ExamRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models;
+using Infrastructure.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,45 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories.Implementations
 {
-    internal class ExamRepository
+    public class ExamRepository : IExamRepository
     {
+        private readonly AppDbContext _context;
+
+        public ExamRepository(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public int AddExam(Exam exam)
+        {
+            _context.Exams.Add(exam);
+            return _context.SaveChanges();
+
+        }
+
+        public int UpdateExam(Exam exam)
+        {
+            Exam ToBeUpdatedExam = _context.Exams.FirstOrDefault(e => e.Eid == exam.Eid);
+            if (ToBeUpdatedExam.TotalMarks != exam.TotalMarks)
+            {
+                ToBeUpdatedExam.TotalMarks = exam.TotalMarks;
+            }
+            else if (ToBeUpdatedExam.TotalQuestions != exam.TotalQuestions)
+            {
+                ToBeUpdatedExam.TotalQuestions = exam.TotalQuestions;
+            }
+            return _context.SaveChanges();
+        }
+        public List<Exam> GetExams()
+        {
+            return _context.Exams.ToList();
+        }
+
+        public Exam GetExamById(int examId)
+        {
+            return _context.Exams.FirstOrDefault(e => e.Eid == examId);
+        }
     }
 }
+
+
