ecd65f5f24c2b9a2be2d29038892d3133cd79552
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-09-10 16:07:57 +0530

    create: scaffolded models

---
+++ Domain/Models/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models;
+
+public partial class Question
+{
+    public int Qid { get; set; }
+
+    public int? Sid { get; set; }
+
+    public int? Eid { get; set; }
+
+    public string? Type { get; set; }
+
+    public string? Question1 { get; set; }
+
+    public decimal? Marks { get; set; }
+
+    public string? Options { get; set; }
+
+    public int? ApprovalStatus { get; set; }
+
+    public virtual Exam? EidNavigation { get; set; }
+
+    public virtual ICollection<QuestionReport> QuestionReports { get; set; } = new List<QuestionReport>();
+
+    public virtual ICollection<Response> Responses { get; set; } = new List<Response>();
+
+    public virtual Subject? SidNavigation { get; set; }
+}
---
+++ Domain/Models/Response.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models;
+
+public partial class Response
+{
+    public int Eid { get; set; }
+
+    public int Qid { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public string? Resp { get; set; }
+
+    public decimal? RespScore { get; set; }
+
+    public virtual Exam EidNavigation { get; set; } = null!;
+
+    public virtual Question QidNavigation { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
