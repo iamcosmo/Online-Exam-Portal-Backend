90950e142c887058c82a2b2f54c0f9bf047bc78d
chandancosmos <chandankumar.pandey@cognizant.com>
2025-09-10 15:01:23 +0530

    Questions: Adding Enums and Models

---
+++ Domain/Models/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Question
+    {
+        [Key]
+        public required string QID { get; set; }
+
+        public required string SID { get; set; }
+
+        public required string EID { get; set; }
+
+        public string Type { get; set; }
+
+        public required string QuestionText { get; set; }
+
+        public decimal Marks { get; set; }
+
+        public required string Options { get; set; }
+
+        public required int ApprovalStatus { get; set; }
+
+    }
+}
