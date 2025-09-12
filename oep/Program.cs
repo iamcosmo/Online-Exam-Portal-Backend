7caf930122581a86ee6571d945b65e60139dd181
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-09-12 09:16:54 +0530

    updated : examscontroller

--- oep/Program.cs
+++ oep/Program.cs
@@ -1,8 +1,13 @@
 
+using Domain.Data;
 using Domain.Models;
 using Infrastructure.Repositories.Implementations;
 using Infrastructure.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace oep
 {
@@ -10,38 +15,64 @@ namespace oep
     {
         public static void Main(string[] args)
         {
-            try{var builder = WebApplication.CreateBuilder(args);
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddScoped<IExamRepository,ExamRepository>();
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddControllers();
+                // Add services to the container.
+                builder.Services.AddScoped<IExamRepository, ExamRepository>();
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                builder.Services.AddControllers();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+                var JwtKey = builder.Configuration.GetValue<string>("Jwt:Secret");
 
-            var app = builder.Build();
+                //builder.Services.AddAuthentication(x =>
+                //{
+                //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
+                //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                //})
+                //   .AddJwtBearer(x =>
+                //   {
+                //       x.RequireHttpsMetadata = true;
+                //       x.SaveToken = true;
+                //       x.TokenValidationParameters = new TokenValidationParameters
+                //       {
+                //           ValidateIssuerSigningKey = true,
+                //           IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtKey)),
+
+                //           ValidateIssuer = true,
+                //           ValidateAudience = true,
+                //           ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                //           ValidAudience = builder.Configuration["Jwt:Audience"],
+                //       };
+                //   });
+
+
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                //if (app.Environment.IsDevelopment())
+                //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            //}
+                //}
 
 
-            app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+                app.UseAuthorization();
 
 
-            app.MapControllers();
+                app.MapControllers();
 
                 app.Run();
             }
-            catch (Exception e){ Console.WriteLine(e); }
+            catch (Exception e) { Console.WriteLine(e); }
         }
 
     }
