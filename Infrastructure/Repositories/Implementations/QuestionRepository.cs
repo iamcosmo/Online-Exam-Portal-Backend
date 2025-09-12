c79b9552fdb64622abce23ff979a4491a8db1a69
chandancosmos <chandankumar.pandey@cognizant.com>
2025-09-12 14:31:12 +0530

    Question repos Init

---
+++ Infrastructure/Repositories/Implementations/QuestionRepository.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Data;
+using Domain.Models;
+using Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly AppDbContext _context;
+
+        public QuestionRepository(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<int> AddQuestion(Question question, Exam exm)
+        {
+            question.Eid = exm.Eid;
+
+            await _context.Questions.AddAsync(question);
+            return await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<List<Question>> GetQuestionsByExamId(int examId)
+        {
+            List<Question> questions = await _context.Questions.Where(q => q.Eid == examId).ToListAsync();
+            return questions;
+        }
+
+        public Question GetQuestionById(int questionId)
+        {
+            return _context.Questions.FirstOrDefault(q => q.Qid == questionId);
+
+        }
+
+        public async Task<int> UpdateQuestion(Question qid)
+        {
+            Question ToBeUpdatedQuestion = _context.Questions.FirstOrDefault(q => q.Qid == qid.Qid);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
--- Infrastructure/Repositories/Interfaces/IQuestionRepository.cs
+++ Infrastructure/Repositories/Interfaces/IQuestionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Infrastructure.Repositories.Interfaces
 {
     public interface IQuestionRepository
     {
+        public Task<int> AddQuestion(Question question, Exam eid);
+
+        public Task<List<Question>> GetQuestionsByExamId(int examId);
+
+        public Question GetQuestionById(int questionId);
+
+        public Task<int> UpdateQuestion(Question qid);
+
     }
 }
