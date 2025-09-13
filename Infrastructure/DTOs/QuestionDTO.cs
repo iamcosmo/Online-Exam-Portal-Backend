78ba3c73868352fa18a3f09cd391ad62cacaab07
chandancosmos <chandankumar.pandey@cognizant.com>
2025-09-13 16:58:35 +0530

    Creating Question DTOs

---
+++ Infrastructure/DTOs/QuestionDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTOs
+{
+    public class QuestionDTO
+    {
+        public int eid { get; set; }
+
+        public string type { get; set; }
+
+        public string question { get; set; }
+
+        public decimal marks { get; set; }
+
+        public string options { get; set; }
+
+        public string correctOptions { get; set; }
+
+
+    }
+}
