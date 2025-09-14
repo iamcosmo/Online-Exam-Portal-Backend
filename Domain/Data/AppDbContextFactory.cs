5c3313266a520123a67d9fc70a0085f68efd5990
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-09-14 16:55:07 +0530

    updated gitignore and added migration method

---
+++ Domain/Data/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Domain.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=LTIN487764;Database=OEP_DB;User Id=sa;Password=password-1;Encrypt=False;");
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
+}
