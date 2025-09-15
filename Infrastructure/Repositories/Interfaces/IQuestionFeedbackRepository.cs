cf69ff81388243249d7bb30d2e52bde4769a60af
chandancosmos <chandankumar.pandey@cognizant.com>
2025-09-15 23:30:06 +0530

    QuestionFeedback Repo-Interface added

---
+++ Infrastructure/Repositories/Interfaces/IQuestionFeedbackRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    public interface IQuestionFeedbackRepository
+    {
+
+        public int AddQuestionFeedbackDTO(AddQuestionFeedbackDTO qFeedback);
+
+        public Task<string> GetFeedbackByQuestionId(int qid);
+
+        public Task<int> UpdateQuestionFeedback(AddQuestionFeedbackDTO qFeedback, int qid);
+    }
+}
