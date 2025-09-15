79f63a9edb470f921554c728b479a41558e11248
chandancosmos <chandankumar.pandey@cognizant.com>
2025-09-15 23:25:18 +0530

    AddQuestionFeedBackDTO added

---
+++ Infrastructure/DTOs/AddQuestionFeedback.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTOs
+{
+    public class AddQuestionFeedback
+    {
+        public int qid { get; set; }
+
+        public string feedback { get; set; }
+
+        public int userId { get; set; }
+    }
+}
