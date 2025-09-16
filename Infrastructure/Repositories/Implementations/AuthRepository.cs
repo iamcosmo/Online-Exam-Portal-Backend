8fd728a59030e558917a232ff4e0affba05e5edd
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-09-16 14:12:55 +0530

    update Auth repo

---
+++ Infrastructure/Repositories/Implementations/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Data;
+using Domain.Models;
+using Infrastructure.Repositories.Interfaces;
+
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public int RegisterUser(User user)
+        {
+            _context.Users.Add(user);
+            return _context.SaveChanges();
+        }
+        public User? Login(string email, string password)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+        }
+    }
+}
