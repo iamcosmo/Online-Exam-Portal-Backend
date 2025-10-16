028743958b43d7ab14ee59c94d55b93ceea105c8
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-10-16 23:35:10 +0530

    final update over topics repo

--- Infrastructure/DTOs/TopicDTOs/GetTopicsDTO.cs
+++ Infrastructure/DTOs/TopicDTOs/GetTopicsDTO.cs
@@ -11,5 +11,6 @@ namespace Infrastructure.DTOs.TopicDTOs
         public int? tid { get; set; }
         public string? subject { get; set; }
         public int? approvalStatus { get; set; }
+        public bool? submittedForApproval { get; set; }
     }
 }
