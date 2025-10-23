57244be4d704d9a494e7fef70a2aa5fb17f53db4
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-10-23 14:12:03 +0530

    updates for implmenting conditional email otp verification

---
+++ Infrastructure/DTOs/AuthDTOs/VerifyOtpRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTOs.AuthDTOs
+{
+    public class VerifyOtpRequestDTO
+    {
+        public int otp { get; set; }
+        public bool AllowSuccessMail { get; set; }
+    }
+}
