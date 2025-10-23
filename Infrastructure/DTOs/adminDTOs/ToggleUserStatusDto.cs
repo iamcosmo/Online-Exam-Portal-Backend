5831b445654bef5e9cef3c35b118a5cace2ff377
Aditya Pathak <2439797@cognizant.com>
2025-10-23 18:29:07 +0530

    manage user

---
+++ Infrastructure/DTOs/adminDTOs/ToggleUserStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DTOs.adminDTOs
+{
+    public class ToggleUserStatusDto
+    {
+        public int UserId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
